59fb2f24d88153715cb04a7527aa045e45f228c0
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-02-09 04:05:18 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jared Thompkins on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
