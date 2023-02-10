ebd6285482b5ff312d7afd5f2987b16f03a907d8
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-02-10 04:27:35 -0600

    initial commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jared Thompkins on 2/10/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
