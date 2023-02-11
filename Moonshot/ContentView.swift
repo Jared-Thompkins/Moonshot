4036be01cf6c19480a3a11906cb84dbad7dc2957
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-02-11 02:55:25 -0600

    add multi-screen navigationlink

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text("Place Holder")
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
